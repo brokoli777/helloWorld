7b8c5d0fd9255768a6e1ace64a5293fe56c6793e
Bregwin Jogi <bregwinpaul@gmail>
2023-01-17 13:44:56 -0500

    "minor editing"

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.send("Hello! I am Bregwin Jogi - 156619215 ");
+    res.send("Bregwin Jogi - 156619215");
 });
 
 // setup http server to listen on HTTP_PORT
