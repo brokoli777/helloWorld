45c6f829283eb5a20a66bc3331c4f3cc2d63ea15
Bregwin Jogi <bregwinpaul@gmail>
2023-01-17 13:47:38 -0500

    "added seneca policy"

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+/*********************************************************************************
+*  WEB322 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Bregwin Jogi Student ID: 156619215 Date: 17-01-2023
+*
+*  Online (Cyclic) URL: https://real-ruby-crow-gear.cyclic.app/
+*
+********************************************************************************/ 
+
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
